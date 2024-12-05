cc342a32ed4504677a0e6f0927bb2dafca15e19c
Axel <emk.disenos@gmail.com>
2024-12-05 17:09:33 -0300

    add physics a jugadores

--- games/Phaser/game1_pong/src/scenes/scene_play.js
+++ games/Phaser/game1_pong/src/scenes/scene_play.js
@@ -1,3 +1,4 @@
+import Jugadores from '../gameObjects/jugadores.js'
 class Scene_play extends Phaser.Scene {
     constructor(){
         super({key: "Scene_play"});
@@ -6,15 +7,29 @@ class Scene_play extends Phaser.Scene {
         //Automatizacion
         let center_width = this.sys.game.config.width/2;
         let center_height = this.sys.game.config.height/2;
+
         //Separador
         this.add.image(center_width, center_height, "separador"); // console.log(this);
-        // Jugador
-        this.izquierda = this.add.image(30, center_height, "izquierda");
-        this.derecha = this.add.image(this.sys.game.config.width-30, center_height, "derecha");
-        //Bola
-        this.ball = this.physics.add.image(center_width, center_height, "ball");
 
+        // Jugador
+        // this.izquierda = this.add.image(30, center_height, "izquierda");
+        // this.derecha = this.add.image(this.sys.game.config.width-30, center_height, "derecha");
+        this.izquierda = new Jugadores(this, 30, center_height, "izquierda");  
+        this.derecha = new Jugadores(this, this.sys.game.config.width-30, center_height, "derecha");
+
+        //Bola
+        this.physics.world.setBoundsCollision(false, false, true, true);
+        this.ball = this.physics.add.image(center_width, center_height, "ball");
+        this.ball.setCollideWorldBounds(true);
+        this.ball.setBounce(1);
         this.ball.setVelocityX(-180);
+
+        //Fisicas
+        this.physics.add.collider(this.ball, this.izquierda, this.tocaJugador, null, this);
+        this.physics.add.collider(this.ball, this.derecha, this.tocaJugador, null, this);
+    }
+    tocaJugador () {
+        
     }
 }
 
