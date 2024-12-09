8955f5de160e6675fb42766f90dd04b10f9419fa
Axel <emk.disenos@gmail.com>
2024-12-09 18:18:07 -0300

    Add animation sprintesheet

---
+++ games/animacion/src/app-bkp.js
@@ -0,0 +1,53 @@
+const config = {
+    width: 320,
+    height: 180,
+    parent: 'container',
+    pixelArt: true,
+    scene: {
+        preload,
+        create
+    }
+}
+
+new Phaser.Game(config);
+
+function preload() {
+    console.log("Preload");
+    this.load.atlas('tomato_atlas', './assets/evil_tomato.png', './assets/evil_tomato.json');
+    this.load.json('evil_tomato_anim', './assets/evil_tomato_anim.json');
+}
+function create() {
+    console.log("Create");
+    this.tomato = this.add.sprite(100, 150, 'tomato_atlas').setScale(5);
+    this.dataAnim = this.cache.json.get('evil_tomato_anim'); 
+
+    this.anims.fromJSON(this.dataAnim);
+
+    this.tomato.anims.play('tomato_atlas_walk');
+
+    // Métodos para parar y continuar la animación
+    // Pausa animación
+    setTimeout(() => {
+        this.tomato.anims.pause();
+    }, 2000);
+
+    // Continua animación
+    setTimeout(() => {
+        this.tomato.anims.resume();
+    }, 4000);
+
+    // Detiene completamente la animación
+    setTimeout(() => {
+        this.tomato.anims.stop();
+    }, 6000);
+
+
+}
+
+
+
+
+
+
+
+
--- games/animacion/src/app.js
+++ games/animacion/src/app.js
@@ -13,35 +13,26 @@ new Phaser.Game(config);
 
 function preload() {
     console.log("Preload");
-    this.load.atlas('tomato_atlas', './assets/evil_tomato.png', './assets/evil_tomato.json');
-    this.load.json('evil_tomato_anim', './assets/evil_tomato_anim.json');
+    this.load.spritesheet('evil_tomato', './assets/evil_tomato.png', {frameWidth: 16, frameHeight: 25});
+    // this.load.json('evil_tomato_anim', './assets/evil_tomato_anim.json');
 }
 function create() {
     console.log("Create");
-    this.tomato = this.add.sprite(100, 150, 'tomato_atlas').setScale(5);
-    this.dataAnim = this.cache.json.get('evil_tomato_anim'); 
+    this.tomato = this.add.sprite(100, 100, 'evil_tomato');
+    // this.tomato = this.add.sprite(100, 100, 'evil_tomato', 0).setScale(5);
 
-    this.anims.fromJSON(this.dataAnim);
+    this.anims.create({
+        key: 'tomato_walk',
+        frames: this.anims.generateFrameNumbers('evil_tomato', {
+            // frames: [ 1, 2, 3, 4, 5, 6, 7, 8]
+            start: 1,
+            end: 8,
+        }), 
+        repeat: -1,
+        frameRate: 12, 
+    });
 
-    this.tomato.anims.play('tomato_atlas_walk');
-
-    // Métodos para parar y continuar la animación
-    // Pausa animación
-    setTimeout(() => {
-        this.tomato.anims.pause();
-    }, 2000);
-
-    // Continua animación
-    setTimeout(() => {
-        this.tomato.anims.resume();
-    }, 4000);
-
-    // Detiene completamente la animación
-    setTimeout(() => {
-        this.tomato.anims.stop();
-    }, 6000);
-
-
+    this.tomato.anims.play('tomato_walk');
 }
 
 
